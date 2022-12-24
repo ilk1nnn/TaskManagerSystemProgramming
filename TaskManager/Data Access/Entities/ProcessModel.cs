3a07af04f51b614cb59dcdcbf4c5b3e499d11dfe
Sule_jr92 <Sule_jr92@STHQ012B-02>
2022-12-24 17:29:20 +0400

    Add project files.

---
+++ TaskManager/Data Access/Entities/ProcessModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManager.Data_Access.Entities
+{
+    public class ProcessModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Machine { get; set; }
+        public int HandleCount { get; set; }
+
+    }
+}
---
+++ TaskManager/Domain/ViewModels/MainViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManager.Domain.Entities;
+
+namespace TaskManager.Domain.ViewModels
+{
+    public class MainViewModel : BaseViewModel
+    {
+
+		private ObservableCollection<ProcessModel> allProcesses;
+
+		public ObservableCollection<ProcessModel> AllProcesses
+        {
+			get { return allProcesses; }
+			set { allProcesses = value; OnPropertyChanged(); }
+		}
+
+
+		public MainViewModel()
+		{
+
+			AllProcesses = new ObservableCollection<ProcessModel>();
+
+
+		}
+
+
+	}
+}
